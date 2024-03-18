3d5ced1e003f9283e1b8a0f1a1df82f47d59f136
IlliaRohalskyi <2004lion2004@gmail.com>
2024-03-18 01:04:51 +0100

    minor changes

--- data_extractor.py
+++ data_extractor.py
@@ -1,10 +1,4 @@
 from PyFoam.Applications.Runner import Runner
+import subprocess
 
-# Replace 'path/to/your/OpenFOAM/case' with the actual path to your case folder
-case_folder = 'cavity'
-
-# Initialize PyFoamRunner
-runner = Runner(args=["pisoFoam", "/asdsaasd/", "cavity"])
-
-# Run the OpenFOAM commands
-runner.start()
+runner = Runner(args=["pisoFoam", "-case", "cavity"])
